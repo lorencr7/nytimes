532534e92543ac6bfbb5c1b55056e94b846ba222
lorenzo villarroel perez <villarroel.lorenzo@hotmail.com>
2018-04-08 14:31:58 +0200

    commit de proyecto

---
+++ nytimes/nytimes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nytimes
+//
+//  Created by lorenzo villarroel perez on 8/4/18.
+//  Copyright © 2018 lorenzo villarroel perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
